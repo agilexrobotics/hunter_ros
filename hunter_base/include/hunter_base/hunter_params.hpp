0f3af6da86f640a815c7f8f5ada798cc3528fe54
zhoups <1013700083@qq.com>
2021-08-31 09:38:28 +0800

    modify hunter2.0 Physical parameters

--- hunter_base/include/hunter_base/hunter_params.hpp
+++ hunter_base/include/hunter_base/hunter_params.hpp
@@ -16,18 +16,18 @@ namespace westonrobot {
 /* hunter Parameters */
 struct HunterParams {
   static constexpr double track =
-      0.576;  // in meter (left & right wheel distance)
+      0.605;  // in meter (left & right wheel distance)
   static constexpr double wheelbase =
-      0.648;  // in meter (front & rear wheel distance)
+      0.650;  // in meter (front & rear wheel distance)
   static constexpr double wheel_radius = 0.165;              // in meter
   static constexpr double transmission_reduction_rate = 30;  // 1:30
 
   // from user manual v1.2.6_S P4
   // max linear velocity: 1.5 m/s
   static constexpr double max_steer_angle =
-      0.582;  // in rad, 0.75 for inner wheel
+      0.58;  // in rad, 0.75 for inner wheel
   static constexpr double max_steer_angle_central =
-      0.576;  // max central angle
+      0.461;  // max central angle
   static constexpr double max_linear_speed = 1.5;  // in m/ss
 };
 }  // namespace westonrobot
