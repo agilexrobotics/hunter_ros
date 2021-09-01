baa056405e44957ba32ea34af044c8613c1a0c94
zhoups <1013700083@qq.com>
2021-09-01 15:01:26 +0800

    fix bug

--- hunter_base/src/hunter_base_node.cpp
+++ hunter_base/src/hunter_base_node.cpp
@@ -63,6 +63,7 @@ int main(int argc, char **argv) {
 
   // connect to robot and setup ROS subscription
   if (port_name.find("can") != std::string::npos) {
+    robot->Connect(port_name);
     robot->EnableCommandedMode();
     ROS_INFO("Using CAN bus to talk with the robot: %s", port_name.c_str());
   } else {
